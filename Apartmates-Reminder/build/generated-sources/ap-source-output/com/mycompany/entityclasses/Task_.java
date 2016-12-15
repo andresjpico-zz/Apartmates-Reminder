3f19a50da6f2f599f4e386e6f2e1297750316ee9
AndresPico <apicochavez@mail.usf.edu>
2016-12-15 04:49:10 -0500

    no message

--- Apartmates-Reminder/build/generated-sources/ap-source-output/com/mycompany/entityclasses/Task_.java
+++ Apartmates-Reminder/build/generated-sources/ap-source-output/com/mycompany/entityclasses/Task_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-15T04:17:45")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-15T04:48:06")
 @StaticMetamodel(Task.class)
 public class Task_ { 
 
