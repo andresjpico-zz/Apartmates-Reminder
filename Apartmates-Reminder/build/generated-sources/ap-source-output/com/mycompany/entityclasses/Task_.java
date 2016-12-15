fa4946e43f63478aacf18bff59aeb16e8aac8842
AndresPico <apicochavez@mail.usf.edu>
2016-12-15 04:46:12 -0500

    no message

---
+++ Apartmates-Reminder/build/generated-sources/ap-source-output/com/mycompany/entityclasses/Task_.java
@@ -0,0 +1,24 @@
+package com.mycompany.entityclasses;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-15T04:17:45")
+@StaticMetamodel(Task.class)
+public class Task_ { 
+
+    public static volatile SingularAttribute<Task, String> taskLocation;
+    public static volatile SingularAttribute<Task, Date> taskDeadline;
+    public static volatile SingularAttribute<Task, String> taskPriority;
+    public static volatile SingularAttribute<Task, String> taskName;
+    public static volatile SingularAttribute<Task, Date> taskReminder1;
+    public static volatile SingularAttribute<Task, Date> taskReminder2;
+    public static volatile SingularAttribute<Task, Date> taskReminder3;
+    public static volatile SingularAttribute<Task, Integer> taskID;
+    public static volatile SingularAttribute<Task, Integer> apartmentID;
+    public static volatile SingularAttribute<Task, String> taskDetails;
+    public static volatile SingularAttribute<Task, String> taskIsComplete;
+
+}
